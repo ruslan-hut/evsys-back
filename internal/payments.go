71d5f215ee0b6b6488f315c8233a9cb665ffecb8
programador <rkhut@h2plt.energy>
2023-08-04 12:54:52 +0200

    add: check transaction id to continue with order

--- internal/payments.go
+++ internal/payments.go
@@ -236,9 +236,11 @@ func (p *Payments) SetOrder(user *models.User, data []byte) (*models.PaymentOrde
 	if order.Order == 0 {
 
 		// if payment process was interrupted, continue with the same order
-		continueOrder, _ := p.database.GetPaymentOrderByTransaction(order.TransactionId)
-		if continueOrder != nil {
-			return continueOrder, nil
+		if order.TransactionId > 0 {
+			continueOrder, _ := p.database.GetPaymentOrderByTransaction(order.TransactionId)
+			if continueOrder != nil {
+				return continueOrder, nil
+			}
 		}
 
 		lastOrder, _ := p.database.GetLastOrder()
