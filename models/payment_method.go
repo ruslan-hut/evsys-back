27718b9842bdd8f945bde23b80631df24d3514a2
programador <rkhut@h2plt.energy>
2023-11-20 12:00:24 +0100

    updated payment method model

--- models/payment_method.go
+++ models/payment_method.go
@@ -11,4 +11,5 @@ type PaymentMethod struct {
 	IsDefault   bool   `json:"is_default" bson:"is_default"`
 	UserId      string `json:"user_id" bson:"user_id"`
 	UserName    string `json:"user_name" bson:"user_name"`
+	FailCount   int    `json:"fail_count" bson:"fail_count"`
 }
