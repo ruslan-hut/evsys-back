cc2d0e800cf3d68df6cf01e826c22d35b3330802
programador <rkhut@h2plt.energy>
2023-11-20 13:11:49 +0100

    add: payment plans, plan data in transaction

--- models/user.go
+++ models/user.go
@@ -7,6 +7,7 @@ type User struct {
 	Password       string    `json:"password" bson:"password"`
 	Name           string    `json:"name" bson:"name"`
 	Role           string    `json:"role" bson:"role"`
+	PaymentPlan    string    `json:"payment_plan" bson:"payment_plan"`
 	Token          string    `json:"token" bson:"token"`
 	UserId         string    `json:"user_id" bson:"user_id"`
 	DateRegistered time.Time `json:"date_registered" bson:"date_registered"`
