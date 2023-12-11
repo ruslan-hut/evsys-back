cd31c90b70f3b30c3eaecb82360263dba465f4fa
programador <rkhut@h2plt.energy>
2023-12-11 12:42:33 +0100

    add: connector data according to a database structure

--- models/connector.go
+++ models/connector.go
@@ -1,10 +1,16 @@
 package models
 
+import "time"
+
 type Connector struct {
-	Id            int    `json:"connector_id" bson:"connector_id"`
-	ChargePointId string `json:"charge_point_id" bson:"charge_point_id"`
-	Type          string `json:"type" bson:"type"`
-	Status        string `json:"status" bson:"status"`
-	Power         int    `json:"power" bson:"power"`
-	TransactionId int    `json:"current_transaction_id" bson:"current_transaction_id"`
+	Id            int       `json:"connector_id" bson:"connector_id"`
+	ChargePointId string    `json:"charge_point_id" bson:"charge_point_id"`
+	Type          string    `json:"type" bson:"type"`
+	Status        string    `json:"status" bson:"status"`
+	StatusTime    time.Time `json:"status_time" bson:"status_time"`
+	Info          string    `json:"info" bson:"info"`
+	VendorId      string    `json:"vendor_id" bson:"vendor_id"`
+	ErrorCode     string    `json:"error_code" bson:"error_code"`
+	Power         int       `json:"power" bson:"power"`
+	TransactionId int       `json:"current_transaction_id" bson:"current_transaction_id"`
 }
