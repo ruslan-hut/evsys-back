ceafb2cd8c33f3add277a77eb6d8e6c031bf0db8
programador <rkhut@h2plt.energy>
2024-04-17 16:05:46 +0200

    changed field name location - coordinates

--- models/location.go
+++ models/location.go
@@ -8,7 +8,7 @@ type Location struct {
 	City              string         `json:"city" bson:"city" validate:"required,max=45"`
 	PostalCode        string         `json:"postal_code" bson:"postal_code" validate:"required,max=10"`
 	Country           string         `json:"country" bson:"country" validate:"required,iso3166_1_alpha3"`
-	Coordinates       GeoLocation    `json:"location" bson:"location" validate:"required"`
+	Coordinates       GeoLocation    `json:"coordinates" bson:"coordinates" validate:"required"`
 	PowerLimit        int            `json:"power_limit" bson:"power_limit" validate:"required"`
 	DefaultPowerLimit int            `json:"default_power_limit" bson:"default_power_limit" validate:"required"`
 	Evses             []*ChargePoint `json:"evses,omitempty" bson:"evses,omitempty" validate:"omitempty"`
