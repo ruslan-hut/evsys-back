46470f63d0adf250a6874de2da6fdec698c8284b
ruslanhut <dev@programmer.com.ua>
2024-09-05 13:37:05 +0200

    changed pong response

--- internal/api/http/server.go
+++ internal/api/http/server.go
@@ -372,9 +372,9 @@ func (c *Client) readPump() {
 		case entity.ListenChargePoints:
 			c.setSubscription(ChargePointEvent)
 		case entity.PingConnection:
-			c.sendResponse(entity.Success, fmt.Sprintf("ping %s", time.Now()))
+			c.sendResponse(entity.Success, fmt.Sprintf("pong %s", c.id))
 		default:
-			c.sendResponse(entity.Success, "request handled")
+			c.sendResponse(entity.Error, fmt.Sprintf("unknown command: %s", userRequest.Command))
 		}
 
 	}
