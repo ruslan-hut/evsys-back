257d8fdb58b9b19cc51b521b977fcda5defb7a39
el hombre <ruslan.hoot@icloud.com>
2024-11-02 13:57:16 +0100

    test monthly report pipeline

--- impl/database/entity.go
+++ impl/database/entity.go
@@ -1,9 +1,9 @@
 package database
 
 type ReportLineMonth struct {
-	Year    int `json:"year"`
-	Month   int `json:"month"`
-	Total   int `json:"totalConsumedWatts"`
-	Count   int `json:"count"`
-	Average int `json:"avgWatts"`
+	Year    int    `json:"year"`
+	Month   int    `json:"month"`
+	Total   int    `json:"totalConsumed"`
+	Count   int    `json:"count"`
+	Average string `json:"avgWatts"`
 }
