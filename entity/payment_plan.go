91c73c6dd284edc628577ac035e7da02f171be02
programador <rkhut@h2plt.energy>
2025-09-22 14:51:48 +0200

    add: new fields to PaymentPlan entity (description, start_time, end_time)

--- entity/payment_plan.go
+++ entity/payment_plan.go
@@ -2,8 +2,11 @@ package entity
 
 type PaymentPlan struct {
 	PlanId       string `json:"plan_id" bson:"plan_id"`
-	IsDefault    bool   `json:"is_default" bson:"is_default"`
+	Description  string `json:"description" bson:"description"`
+	IsDefault    bool   `json:"is_default" bson:"is_default"` // global default, for all users
 	IsActive     bool   `json:"is_active" bson:"is_active"`
 	PricePerKwh  int    `json:"price_per_kwh" bson:"price_per_kwh"`
 	PricePerHour int    `json:"price_per_hour" bson:"price_per_hour"`
+	StartTime    string `json:"start_time" bson:"start_time"`
+	EndTime      string `json:"end_time" bson:"end_time"`
 }
